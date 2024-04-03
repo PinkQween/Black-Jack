da6d9a0c9c5793cdd1d6fa7e09409c95c0d58c8f
Hanna Skairipa <83796834+PinkQween@users.noreply.github.com>
2024-04-03 13:16:41 -0600

    Initial Commit

---
+++ Black Jack/Black_JackApp.swift
@@ -0,0 +1,17 @@
+//
+//  Black_JackApp.swift
+//  Black Jack
+//
+//  Created by Boone, Hanna - Student on 4/3/24.
+//
+
+import SwiftUI
+
+@main
+struct Black_JackApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Black Jack/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Black Jack
+//
+//  Created by Boone, Hanna - Student on 4/3/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
